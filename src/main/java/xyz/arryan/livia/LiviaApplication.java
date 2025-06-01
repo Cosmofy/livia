7e19b93ecdb0109042e12e5e0f3cc6b74db8d2d8
Arryan Bhatnagar <apb6254@psu.edu>
2025-06-01 19:43:51 +0400

    initial commit

---
+++ src/main/java/xyz/arryan/livia/LiviaApplication.java
@@ -0,0 +1,13 @@
+package xyz.arryan.livia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiviaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiviaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/arryan/livia/LiviaApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.arryan.livia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiviaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
